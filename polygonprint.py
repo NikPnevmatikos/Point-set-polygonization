20de8d762fb3446692286677e4208e5c6e66525b
NikPnevmatikos <sdi1900157@di.uoa.gr>
2022-11-04 15:32:10 +0200

    added script for showing polygon in a graph

--- polygonprint.py
+++ polygonprint.py
@@ -1,39 +1,23 @@
 import matplotlib.pyplot as plt
   
 
-with open("./results/test3.txt", "r") as file:
+with open("./results/test4.txt", "r") as file:
     points_x=[]
     points_y=[]
-    fl=False
-    turn=0
-    flag=False
     points=[]
     for str in file:
-        if not flag:
-            if str[0]=="-":
-                flag=True
-                continue
+
+            if str[0]=="A":
+                break
+            
             list=str.split(" ")
-            pointx=float(list[0])
-
-            pointy=float(list[1][:-1])
-            points.append([pointx,pointy])
-        if flag:
-            if str[0]=="-":
-                turn+=1
-                fl=True
-                print(turn)
-                plt.plot(points_x,points_y)
-                for point in points:
-                    plt.plot(point[0],point[1],"o-")
-                plt.show()
-                points_x.clear()
-                points_y.clear()
-            else:
-                fl=False
-                list=str.split(" ")
+                        
+            if(len(list) == 2):      
+                pointx=float(list[0])
+                pointy=float(list[1][:-1])
+                points.append([pointx,pointy])
+            elif(len(list) == 4):
                 point1x=list[0]
-
                 point1y=list[1]
                 point2x=list[2]
                 point2y=list[3][:-1]
@@ -45,7 +29,7 @@ with open("./results/test3.txt", "r") as file:
                 
                 points_x.append(float(point2[0]))
                 points_y.append(float(point2[1]))
-    if (not fl):
-        plt.plot(points_x,points_y)
-        plt.show()
-        print(points_x)
+
+    plt.plot(points_x,points_y)
+    plt.show()
+    print(points_x)
