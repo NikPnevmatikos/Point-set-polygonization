f759d42e48ac30423b5d5b5ca2dd806fe0ccbe34
NikPnevmatikos <sdi1900157@di.uoa.gr>
2022-12-05 22:19:28 +0200

    optimal polygon

--- exe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cgal_create_CMakeLists -s main
-cmake -DCGAL_DIR=/usr/lib/CGAL .
-make
-./main -edge_selection 3 -initialization 1b -algorithm convex_hull -i testcases/test4.txt -o results/test4.txt
